94e9b3fe8d45bbbf06657bd081d21a5f254c22c3
Nazim Bayraktar <nab1079@hotmail.com>
2023-10-19 13:32:31 +0100

    Added second  class="main-color" on <main><div> index.html. Added functuality for fetching color data from IPA and display the color on the page with hex code in the bottom

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
-const toggle = document.getElementById('toggle')
-
+const toggleBtn = document.getElementById('toggle')
 const container = document.querySelector('.container')
-
 const select = document.querySelector('select')
-
-const getBtn = document.getElementById('get-color')
-
+const inputColor = document.getElementById('input')
+const getColorBtn = document.getElementById('get-color')
 const hexValues = document.querySelectorAll('.hex-value')
-
 const footerText = document.querySelector('.attribution')
+const mainColor = document.querySelectorAll('.main-color')
+let colorData
 
 
-toggle.addEventListener('click', toggleDarkLight)
-
+toggleBtn.addEventListener('click', toggleDarkLight)
+getColorBtn.addEventListener('click', getColorScheme)
 
 function toggleDarkLight() {
   let element = document.body
   element.classList.toggle('light-mode')
-
   container.classList.toggle('container-dark')
-
   select.classList.toggle('select-dark')
-
-  getBtn.classList.toggle('btn-dark')
-
+  getColorBtn.classList.toggle('btn-dark')
   for (hex of hexValues) {
     hex.classList.toggle('hex-value-light')
   }
-
   footerText.classList.toggle('attribution-dark')
+}
+
+function getColorScheme() {
+  const hexValue = inputColor.value.slice(1)
+  const selectedValue = select.value
+
+  fetch(`https://www.thecolorapi.com/scheme?hex=${hexValue}&mode=${selectedValue}`)
+    .then(res => res.json())
+    .then(data => {
+      colorData = data.colors
+      updateColors()
+    })
+}
+
+function updateColors() {
+  for (color of colorData) {
+    const index = colorData.indexOf(color)
+    const hexVal = color.hex.value
+    mainColor[index].style.backgroundColor = `${hexVal}`
+    hexValues[index].textContent = `${hexVal}`
+  }
 }
